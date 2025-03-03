08272675bd44872decbab568a23915b175cb3b3b
Proties <124195018+Proties@users.noreply.github.com>
2025-03-03 16:14:43 +0200

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,35 @@
+import "./Navbar.scss"
+
 function Navbar(){
+    return(
     <nav>
-        <div className="left">Left</div>
-        <div className="right">Right</div>
+        <div className="left">
+            <a href="/" className="logo">
+                <img src="/logo.png" alt="" />
+                <span>LamaEstate</span>
+            </a>
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+            <a href="/">Agents</a>
+        </div>
+        <div className="right">
+            <a href="/">Sign In</a>
+            <a href="/" className="register">Sign Up</a>
+            <div className="menuIcon">
+                <img src="/menu.png" alt="" />
+            </div>
+            <div className="menu">
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+                <a href="/">Sign in</a>
+                <a href="/">Sign up</a>
+            </div>
+        </div>
     </nav>
+    )
 }
 
 export default Navbar;
