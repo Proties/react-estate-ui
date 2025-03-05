86ee47029581d266dda14fca8f1c08e090cb8692
Proties <124195018+Proties@users.noreply.github.com>
2025-03-05 13:42:06 +0200

    Layout update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import Navbar from "./components/navbar/Navbar"
+import { createBrowserRouter, RouterProvider, } from "react-router-dom"
 import "./layout.scss"
 import HomePage from "./routes/homePage/homePage"
+import ListPage from "./routes/listPage/listPage"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:(
+      <HomePage />
+    ),
+  },
+  {
+    path: "/List",
+    element: <ListPage />,
+  },
+]
+)
+
 
 function App() {
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar/>
-      </div>
-      <div className="content">
-       <HomePage/>
-      </div>
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navbar/>
+    //   </div>
+    //   <div className="content">
+    //    <HomePage/>
+    //   </div>
       
-    </div>
+    // </div>
+
+    <RouterProvider router ={router}/>
   )
 }
 
