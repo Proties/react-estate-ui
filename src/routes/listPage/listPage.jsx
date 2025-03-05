875bfebfda639254cdaa1051404e952791864e30
Proties <124195018+Proties@users.noreply.github.com>
2025-03-05 15:00:14 +0200

    filter update

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,11 +1,16 @@
-import './listPage.scss'
+import { listData } from "../../lib/dummydata"
+import './listPage.scss';
 
 function ListPage(){
-    const data = listData
+    const data = listData;
 
     return(
         <div className='listPage'>
-            <div className="listContainer">List</div>
+            <div className="listContainer">
+                <div className="wrapper">
+                    
+                </div>
+            </div>
             <div className="mapContainer">Map</div>
 
         </div>
